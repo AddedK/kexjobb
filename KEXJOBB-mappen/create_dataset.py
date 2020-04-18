07a61a5b8c2d4340a5f472d90cab788f96426451
Sabrina Chowdhury <sabcho@kth.se>
2020-04-18 18:39:19 +0200

    Update create_dataset.py

--- KEXJOBB-mappen/create_dataset.py
+++ KEXJOBB-mappen/create_dataset.py
@@ -1,13 +1,14 @@
 import numpy as np
 import fileinput
-import glob
+
 
 nr_of_labels = [109,452,856,459,57,1875]
 label_names = ["glutamatergic","granule", "medium_spiny", "basket", "fast_spiking", "pyramidal"]
 
-X_train_list = ["glutamatergic_X_train","granule_X_train", "medium_spiny_X_train", "basket_X_train", "fast_spiking_X_train", "pyramidal_X_train"]
+X_train_list = ["glutamatergic_X_train.txt","granule_X_train.txt", "medium_spiny_X_train.txt", "basket_X_train.txt", "fast_spiking_X_train.txt", "pyramidal_X_train.txt"]
 
-labels  = np.zeros(sum(nr_of_labels))
+#labels  = np.zeros(sum(nr_of_labels))
+labels = []
 
 def create_labels():
 
@@ -15,8 +16,14 @@ def create_labels():
         for i in range(label_amount):
             labels.append(label_name)
 
-def create_dataset():
+    np.savetxt("labels.txt", labels,  fmt='%s')
 
+def create_dataset():
     with open('X_train_all_types.txt', 'w') as file:
         input_lines = fileinput.input(X_train_list)
         file.writelines(input_lines)
+
+
+create_labels()
+print(labels)
+#create_dataset()
