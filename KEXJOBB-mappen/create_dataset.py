a71187467533d276dfc5b4152f2182aa8629739b
Sabrina Chowdhury <sabcho@kth.se>
2020-04-18 17:38:45 +0200

    Create create_dataset.py

---
+++ KEXJOBB-mappen/create_dataset.py
@@ -0,0 +1,22 @@
+import numpy as np
+import fileinput
+import glob
+
+nr_of_labels = [109,452,856,459,57,1875]
+label_names = ["glutamatergic","granule", "medium_spiny", "basket", "fast_spiking", "pyramidal"]
+
+X_train_list = ["glutamatergic_X_train","granule_X_train", "medium_spiny_X_train", "basket_X_train", "fast_spiking_X_train", "pyramidal_X_train"]
+
+labels  = np.zeros(sum(nr_of_labels))
+
+def create_labels():
+
+    for label_amount, label_name in zip(nr_of_labels,label_names):
+        for i in range(label_amount):
+            labels.append(label_name)
+
+def create_dataset():
+
+    with open('X_train_all_types.txt', 'w') as file:
+        input_lines = fileinput.input(X_train_list)
+        file.writelines(input_lines)
