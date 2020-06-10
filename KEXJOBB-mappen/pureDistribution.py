9a8d49f169949f7404d2a3104a356eb8a86d2f1b
Sabrina Chowdhury <sabcho@kth.se>
2020-06-10 16:59:59 +0200

    added comments for file where distribution for pure clusters is generated

---
+++ KEXJOBB-mappen/pureDistribution.py
@@ -0,0 +1,41 @@
+import sys
+import csv
+import numpy as np
+import matplotlib.cm as cm
+from tmd.view import view, plot
+import tmd
+import matplotlib.pyplot as plt
+from tmd.view.common import jet_map
+import os
+
+
+wardPureClusters = np.genfromtxt(
+    "dim_wardPureClusters.txt", usecols=1, dtype="str")
+
+
+wardPureClusterIndexes = []
+with open("dim_wardPureClusterIndexes.txt") as f:
+    currentList = []
+    for line in f:
+        # print(line)
+        if(line != "\n"):
+            line = line[:-1]
+            currentList.append(float(line))
+        else:
+            wardPureClusterIndexes.append(currentList)
+            currentList = []
+
+
+label_names = ["glutamatergic", "granule",
+               "medium_spiny", "basket", "fast_spiking", "pyramidal"]
+
+typeDistribution = [0] * 6
+for i, cluster in enumerate(wardPureClusterIndexes):
+    if len(cluster) > 1:
+        neuroMorphoName = wardPureClusters[i]
+        index = label_names.index(neuroMorphoName)
+        # print(index)
+        typeDistribution[index] += 1
+
+
+print(typeDistribution)
