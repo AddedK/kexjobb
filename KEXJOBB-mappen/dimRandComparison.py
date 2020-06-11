4436c54549a3c6f14f05331673a71d92341eb54d
Sabrina Chowdhury <sabcho@kth.se>
2020-06-11 09:40:02 +0200

    added comment for ARI calculation code

---
+++ KEXJOBB-mappen/dimRandComparison.py
@@ -0,0 +1,17 @@
+"""
+
+CALCULATES THE ADJUSTED RAND INDEX FOR BOTH CLUSTERINGS
+
+"""
+
+import numpy as np
+from sklearn import metrics
+from sklearn.cluster import AgglomerativeClustering
+from sklearn.cluster import AffinityPropagation
+
+ap_labels = np.loadtxt("dim_ap_labels.txt")
+ward_labels = np.loadtxt("dim_ward_labels.txt")
+
+
+score = metrics.adjusted_rand_score(ap_labels, ward_labels)
+print(score)
